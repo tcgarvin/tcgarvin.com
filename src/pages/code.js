93a9098e9e7589b081a9686f2e65ac818c7c3b68
Tim Garvin <tcgarvin@gmail.com>
2018-09-22 00:16:30 -0400

    Spelling

--- src/pages/code.js
+++ src/pages/code.js
@@ -29,7 +29,7 @@ const CodePage = () => (
           </Showcase>
           <Showcase title="Dots" imageUrl="/images/ucd-thumbnail.png">
             <p>At UrbanCode, there was a period of time where we had a lot of trouble tracking work, especially around customer engagement. It's not that the data wasn't there, it's that the tools we had to navigate and understand that data were extremely limited.</p>
-            <p>Dots was an experiment in data visualization that would scrape the several systems of record that we cared about, and provide intuitive visualizations for our most common questions.  (Typically, "What am I suppose to work on next", "How is everyone doing", "Is anyone falling behind", "Are there any emergency situations brewwing", "Are customer situations staffed correctly", and that sort of thing.</p>
+            <p>Dots was an experiment in data visualization that would scrape the several systems of record that we cared about, and provide intuitive visualizations for our most common questions.  (Typically, "What am I supposed to work on next", "How is everyone doing", "Is anyone falling behind", "Are there any emergency situations brewing", "Are customer situations staffed correctly", and that sort of thing.</p>
             <p>This approach ended up being incredibly effective. The experiment was eventually subsumed into a new UrbanCode product offering.</p>
             <p>Now that Dots is dead, I think a lot about spinning it back up for use with generic spreadsheets.</p>
           </Showcase>
@@ -39,7 +39,7 @@ const CodePage = () => (
           </Showcase>
           <Showcase title="APAR Wizard" imageUrl="/images/ucd-thumbnail.png">
             <p> At IBM, tracking a product bug sometimes required records to be filed in several systems. I wrote a chrome extension to tie together the creation of a few of them.</p>
-            <p> This tool saw excellent adoption in my team.  I like to think it saved weeks of developer time during it's lifetime.</p>
+            <p> This tool saw excellent adoption in my team.  I like to think it saved weeks of developer time during its lifetime.</p>
           </Showcase>
           <Showcase title="Flag Animation" imageUrl="/images/hoc-thumbnail.png" link="http://bl.ocks.org/tcgarvin/6174df7cbdf860d22955">
             <p>I got a little excited about House of Cards a few years ago and spent the day getting a d3-based flag animation just right.</p>
@@ -52,7 +52,7 @@ const CodePage = () => (
           </Showcase>
           <Showcase title="Hashnav" imageUrl="/images/github-thumbnail.png" link="https://github.com/tcgarvin/hashnav">
             <p>Really old side project generating a force-directed layout between related hashtags.  Figured out which hashtags were related by using the free Twitter 1% firehose.</p>
-            <p>Was prety much just an excuse to learn Neo4J.</p>
+            <p>Was pretty much just an excuse to learn Neo4J.</p>
           </Showcase>
         </Masonry>
       </Grid>
