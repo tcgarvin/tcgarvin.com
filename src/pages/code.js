7c055fd085ae084549f280a832a112b41193a5b4
Tim Garvin <tcgarvin@gmail.com>
2020-01-18 12:35:24 -0500

    Assorted updates
    Added Vis Talk and Advent of Code

--- src/pages/code.js
+++ src/pages/code.js
@@ -12,7 +12,7 @@ const CodePage = () => (
       <Row>
         <Col xs={12}>
           <h2>Things I've Done</h2>
-          <p>Time's flown since 2012, and I've picked up a few things along the way. You can browse some of them here if you want. Most of them are regrettably closed-source, but I've linked to Github when possible.</p>
+          <p>Time has flown since I got out of school, and I've picked up a few things along the way. You can browse some of them here if you want. Most of them are regrettably closed-source, but I've linked to Github when possible.</p>
         </Col>
       </Row>
     </Grid>
@@ -24,31 +24,43 @@ const CodePage = () => (
           options={{transitionDuration:0}}
         >
           <Showcase title="Protectus Sentry" imageUrl={withPrefix('/images/protectus-thumbnail.png')} link="https://protectus.com/sentry" >
-            <p>Built this with my dad.  I was did the (Python) web app, he did everything else, and we met in the middle, in MongoDB.</p>
+            <p>Built this with my dad.  I did the (Python) web app, he did everything else, and we met in the middle, in MongoDB.</p>
             <p>There's something to be said for building a product from scratch, and really owning it.</p>
           </Showcase>
           <Showcase title="Dots" imageUrl="/images/ucd-thumbnail.png">
             <p>At UrbanCode, there was a period of time where we had a lot of trouble tracking work, especially around customer engagement. It's not that the data wasn't there, it's that the tools we had to navigate and understand that data were extremely limited.</p>
-            <p>Dots was an experiment in data visualization that would scrape the several systems of record that we cared about, and provide intuitive visualizations for our most common questions.  (Typically, "What am I supposed to work on next", "How is everyone doing", "Is anyone falling behind", "Are there any emergency situations brewing", "Are customer situations staffed correctly", and that sort of thing.</p>
-            <p>This approach ended up being incredibly effective. The experiment was eventually subsumed into a new UrbanCode product offering.</p>
+            <p>Dots was an experiment in data visualization that would scrape the several systems of record that we cared about, and provide intuitive visualizations for our most common questions.  (Typically, "What am I supposed to work on next", "How is everyone doing", "Is anyone falling behind", "Are there any emergency situations brewing", "Are customer situations staffed correctly", and that sort of thing.)</p>
+            <p>This approach ended up being incredibly effective. The experiment was eventually subsumed into a <a href="https://youtu.be/CuDRyRD79Wc?t=65">new UrbanCode product offering.</a></p>
             <p>Now that Dots is dead, I think a lot about spinning it back up for use with generic spreadsheets.</p>
           </Showcase>
           <Showcase title="UrbanCode Deploy" imageUrl={withPrefix('/images/ucd-thumbnail.png')} link="https://developer.ibm.com/urbancode/">
             <p>My friend Matt pulled me onto this team a year or two after their acquisition by IBM. Java, SQL, JMS, Dojo -- a traditional stack.</p>
             <p>I had an opportunity to do just about everything you can do in a large enterprise product team.  From subsystem rewrites to on-site customer troubleshooting sessions to UX design.</p>
           </Showcase>
+          <Showcase title="Visualization Talk" imageUrl="/images/vistalk-thumbnail.png" link="https://slides.com/timgarvin/boss-vis/fullscreen#/">
+            <p>I had the distinct priviledge of presenting an introductory talk at CodeMash 2020 titled "How To Impress Your Boss with Interactive Visualziations".  In it, I gave an overview of how to approach data visualization in Python (stemming from the Jupyter ecosystem) and in Javascript (stemming from SVG).  Talked about Jupyter, Streamlit, D3, and ObservableHQ.</p>
+            <p>The topic was far too broad -- I'll be sure to keep it narrower the next time I apply.  A good experience nonetheless.</p>
+          </Showcase>
           <Showcase title="APAR Wizard" imageUrl="/images/ucd-thumbnail.png">
             <p> At IBM, tracking a product bug sometimes required records to be filed in several systems. I wrote a chrome extension to tie together the creation of a few of them.</p>
             <p> This tool saw excellent adoption in my team.  I like to think it saved weeks of developer time during its lifetime.</p>
           </Showcase>
           <Showcase title="Flag Animation" imageUrl="/images/hoc-thumbnail.png" link="http://bl.ocks.org/tcgarvin/6174df7cbdf860d22955">
-            <p>I got a little excited about House of Cards a few years ago and spent the day getting a d3-based flag animation just right.</p>
+            <p>Back when House of Cards was a big thing, I got a little excited about it and spent the day getting a d3-based flag animation just right.</p>
           </Showcase>
           <Showcase title="Blackjack Simulation" imageUrl="/images/blackjack-thumbnail.png" link={withPrefix("/html/blackjack.html")}>
             <p>Every few years I get into a blackjack rut. (Mentally, not financially) One year I scratched the itch by spiking out a blackjack simulation.</p>
           </Showcase>
+          <Showcase title="Advent of Code" imageUrl="/images/github-thumbnail.png">
+            <p>I've enjoyed participating in Advent of Code for the past few years.  It usually takes me ~100 hours for me to get through it, and is well worth the time to keep your algorithmic mind sharp</p>
+            <ul>
+              <li><a href="https://github.com/tcgarvin/adventOfCode2017">2017</a></li>
+              <li><a href="https://github.com/tcgarvin/adventOfCode2018">2018</a> (Did not finish)</li>
+              <li><a href="https://github.com/tcgarvin/adventOfCode2019">2019</a></li>
+            </ul>
+          </Showcase>
           <Showcase title="Wedding Website" imageUrl="/images/github-thumbnail.png" link="https://github.com/tcgarvin/ashleyandtim.us">
-            <p>Like all good programers, I made my own wedding site.</p>
+            <p>Like all good programers, I made my own wedding site.  Because I set it up with GitHub Pages, is actually still available, which is amazing to me</p>
           </Showcase>
           <Showcase title="Hashnav" imageUrl="/images/github-thumbnail.png" link="https://github.com/tcgarvin/hashnav">
             <p>Really old side project generating a force-directed layout between related hashtags.  Figured out which hashtags were related by using the free Twitter 1% firehose.</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,7 @@ import { Link } from 'gatsby'
 import { withPrefix } from 'gatsby-link'
 
 import Layout from '../components/layout'
-//import TopThing from '../components/topthing'
 import { Grid, Row, Col } from 'react-bootstrap';
-
-
 
 const IndexPage = () => (
   <Layout
@@ -29,11 +26,12 @@ const IndexPage = () => (
     <Grid>
       <Row>
         <Col xs={12} md={8} mdOffset={2}>
-          <p>You've stumbled upon an experimental site that I've put up using <a href="https://www.gatsbyjs.org/">Gatsby</a> and <a href="https://www.netlify.com/">Netlify</a>.  You won't find much content, though there may be some tech demos here and there.</p>
-          <h2 className="under-top=thing">About Me</h2>
-          <p>I'm a rapid prototyper at PricewaterhouseCoopers. Prior to that, I made my living writing DevOps tools at IBM. I was also heavily involved in the creation of the <a href="http://protectus.com/sentry">Protectus Sentry</a>, a product I still love, use, and support.</p>
-          <p>Non-code things I'm involved in include <a href="http://thefest.us">The FEST</a>, getting to know my lovely wife and son, being a studious Catholic, and riding the bus.</p>
-          <p>Check out some <Link to="/code/">side projects and code spikes</Link> that I play around with.</p>
+          <p>You've stumbled upon my personal site.  There's not much content to be seen, aside from a listing of projects below.</p>
+          <h2 className="under-top-thing">About Me</h2>
+          <p>I'm a rapid prototyper at PwC. Prior to that, I made my living developing and brainstorming DevOps tools at IBM.  Straight out of school, my dad and I co-developed the <a href="http://protectus.com/sentry">Protectus Sentry</a>, a product that's still in the wild today.</p>
+          <p>Non-code things I'm involved in include <a href="http://thefest.us">The FEST</a>, getting to know my lovely wife and two sons, being a studious Catholic, and riding the bus.</p>
+          <h2>Things I've Done</h2>
+          <p>Time has flown since I started out, and I've picked up a few things along the way. You can browse some of them on my <Link to="/code">Projects Page</Link>.</p>
         </Col>
       </Row>
     </Grid>
