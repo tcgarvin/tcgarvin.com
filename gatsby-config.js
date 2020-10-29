b49bebf0feba54664ca41b81d923f82136f57e14
Tim Garvin <tcgarvin@gmail.com>
2020-10-28 20:42:14 -0400

    Added simple favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'gatsby-starter-default',
+        icon: 'src/images/favicon.png',
         short_name: 'starter',
         start_url: '/',
         background_color: '#663399',
